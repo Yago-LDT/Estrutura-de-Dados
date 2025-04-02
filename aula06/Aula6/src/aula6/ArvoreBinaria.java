c1f64ae431453a0eb405ec6c7de2bf1a81b14b86
Yago Pereira <yagopereiranunes6@gmail.com>
2025-04-02 11:03:14 -0300

    Update ArvoreBinaria.java

--- aula06/Aula6/src/aula6/ArvoreBinaria.java
+++ aula06/Aula6/src/aula6/ArvoreBinaria.java
@@ -28,4 +28,48 @@ public class ArvoreBinaria {
             inOrder(raiz.direita);
         }
     }
+
+     public No buscar(No raiz, int chave) {
+    if (raiz == null || raiz.valor == chave) {
+        return raiz;
+    }
+
+    if (chave < raiz.valor) {
+        return buscar(raiz.esquerda, chave);
+    }
+
+    return buscar(raiz.direita, chave);
+}
+
+    public No remover(No raiz, int valor) {
+    if (raiz == null) {
+        return raiz;
+    }
+
+    if (valor < raiz.valor) {
+        raiz.esquerda = remover(raiz.esquerda, valor);
+    } else if (valor > raiz.valor) {
+        raiz.direita = remover(raiz.direita, valor);
+    } else {
+        if (raiz.esquerda == null) {
+            return raiz.direita;
+        } else if (raiz.direita == null) {
+            return raiz.esquerda;
+        }
+
+        raiz.valor = menorValor(raiz.direita);
+        raiz.direita = remover(raiz.direita, raiz.valor);
+    }
+
+    return raiz;
+}
+
+    private int menorValor(No raiz) {
+    int menor = raiz.valor;
+    while (raiz.esquerda != null) {
+        menor = raiz.esquerda.valor;
+        raiz = raiz.esquerda;
+    }
+    return menor;
+}
 }
