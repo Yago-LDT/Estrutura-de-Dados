a2ebfdc03354551aa51dec1b9e82909fccff7740
Yago Pereira <yagopereiranunes6@gmail.com>
2025-04-02 11:03:33 -0300

    Update Aula6.java

--- aula06/Aula6/src/aula6/Aula6.java
+++ aula06/Aula6/src/aula6/Aula6.java
@@ -2,7 +2,23 @@ package aula6;
 
 public class Aula6 {
     public static void main(String[] args) {
-        
+        ArvoreBinaria arvore = new ArvoreBinaria();
+
+        arvore.inserir(50);
+
+        arvore.inserir(30);
+
+        arvore.inserir(70);
+
+        arvore.inserir(20);
+
+        arvore.inserir(40);
+
+
+
+        System.out.print("Percorrendo a árvore em ordem: ");
+
+        arvore.inOrder(arvore.raiz);
     }
     
 }
